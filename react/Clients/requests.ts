c0a0fff9aa688c6644d810ba844ccd47c92e1808
JerryLion-PC <jeremaios@gmail.com>
2021-08-25 09:58:56 -0300

    export req constants

--- react/Clients/requests.ts
+++ react/Clients/requests.ts
@@ -1,8 +1,10 @@
-export const postRequest = (data: object) =>
-  new Request('https://wfue8x1b1a.execute-api.us-east-1.amazonaws.com', {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+export const BASE_URL =
+  'https://wfue8x1b1a.execute-api.us-east-1.amazonaws.com';
+
+export const postRequest = (data: object) => ({
+  method: 'POST',
+  body: JSON.stringify(data),
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
