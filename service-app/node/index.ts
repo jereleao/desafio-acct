5cefb6cbccc87a9b10c88c8b7063f361c3eaf159
hugooleal <77586151+hugooleal@users.noreply.github.com>
2021-08-28 16:23:33 -0300

    Finalização do service app.

--- service-app/node/index.ts
+++ service-app/node/index.ts
@@ -3,17 +3,20 @@ import { LRUCache, Service } from '@vtex/api'
 
 import { Clients } from './clients'
 import { leadUpdater } from './middlewares/leadUpdater'
-import { getPhone } from './middlewares/getPhone'
+import { getEmail } from './middlewares/getEmail'
 
 
 const TIMEOUT_MS = 5000
 
-//Define o máximo de alocação de cache para o cliente Lead.
+// Define o máximo de alocação de cache para o cliente Lead.
 const memoryCache = new LRUCache<string, any>({ max: 5000 })
 
+// Controla o montante de cache utilizada.
 metrics.trackCache('lead', memoryCache)
+metrics.trackCache('oms', memoryCache)
+metrics.trackCache('profile', memoryCache)
 
-//Define as configurações base dos clientes.
+// Define as configurações base dos clientes.
 const clients: ClientsConfig<Clients> = {
   implementation: Clients,
   options: {
@@ -31,15 +34,15 @@ const clients: ClientsConfig<Clients> = {
 
 declare global {
 
-  //Context type declarada para evitar a necessidade de importar o ServiceContext nos handlers e resolvers.
+  // Context type declarada para evitar a necessidade de importar o ServiceContext nos handlers e resolvers.
   type Context = ServiceContext<Clients, State>
 
-  //Declara a forma do "ctx.state"
+  // Declara a forma do "ctx.state"
   interface State extends RecorderState {
-    phone: string
+    email: string
   }
 
-  //Declara a forma do context de evento a ser utilizado a ser utilizado pelo middleware.
+  // Declara a forma do context de evento a ser utilizado pelo middleware getEmail.
   interface OrderPlacedContext extends EventContext<Clients> {
     body: {
       domain: string
@@ -51,6 +54,7 @@ declare global {
     }
   }
 
+  // Declara a forma do context de evento a ser utilizado pelo middleware leadUpdater.
   interface LeadContext extends EventContext<Clients> {
     body: {
       phone: string
@@ -58,10 +62,10 @@ declare global {
   }
 }
 
-//Exporta o serviço definindo os clientes e eventos.
+// Exporta o serviço definindo os clientes e eventos.
 export default new Service({
   clients,
   events: {
-    getPhone: [getPhone, leadUpdater]
+    getEmail: [getEmail, leadUpdater]
   }
 })
--- service-app/node/middlewares/leadUpdater.ts
+++ service-app/node/middlewares/leadUpdater.ts
@@ -1,19 +1,22 @@
 import { NotFoundError } from "@vtex/api"
 
-//Define o middleware leadUpdater, responsável por requerer a atualização das informações de lead junto à API externa.
+// Define o middleware leadUpdater, responsável por requerer a atualização das informações de lead junto à API externa.
 export async function leadUpdater(
     ctx: LeadContext,
     next: () => Promise<any>
   ) {
+
+    // Recebe o email através do "ctx.state".
     const {
-      state: { phone }
+      state: { email }
     } = ctx
 
-    //Con
-    const id = (await ctx.clients.lead.getItem(phone)).id
+    // Com base no email, faz a requisição do ID da lead na API da AWS.
+    const id = (await ctx.clients.lead.getItem(email)).id
 
+    // Caso não haja cadastro na AWS com o email indicado, interrompe o middleware e avisa que não há usuário cadastrado.
     if (!id) {
-      throw new NotFoundError('Lead não cadastrado')
+      throw new NotFoundError('Lead não cadastrada')
     }
 
     //Monta o body para envio e atualização na base da API da AWS.
@@ -22,9 +25,10 @@ export async function leadUpdater(
       "client_type": "CLIENT"
     }
 
-    //Envia as informações de atualização da lead com base na função putItem do cliente lead.
-    await ctx.clients.lead.putItem(body).
-    then((response) => console.log(response))
+    // Envia as informações de atualização da lead com base na função putItem do cliente lead.
+    ctx.clients.lead.putItem(body).
+    
+    then((response) => console.log(`Lead atualizada para ${response.client_type} com sucesso!`))
     .catch((reason) => console.log('Algo deu errado:', reason?.response?.data))
   
     await next()
